26843d9cb4a2fa64295b0581d6f3c83dce5e92b4
= <ITPS@gmail.com>
2022-04-01 12:52:37 +0900

    Html, CSS, JS 수업

---
+++ instagram/js/signup.js
@@ -0,0 +1,34 @@
+const input_datas = document.querySelectorAll('.input-data');
+const password_visible = document.querySelector('.password-visible')
+
+password_visible.onclick = () => {
+
+}
+
+for (let i = 0; i < input_datas.length; i++) {
+    const label = input_datas[i].querySelector('label');
+    const input = input_datas[i].querySelector('label');
+
+    label.onkeydown = () => {
+        label.style.top = '2px';
+        label.style.fontSize = '11px';
+        input.style.padding = '16px 0px 2px 8px';
+    }
+
+    input.onkeyup = () => {
+        if (input.ariaValueMax.length == 0) {
+            label.style.top = '9px';
+            label.style.fontSize = '13px';
+            input.style.padding = '9px 0px 7px 8px';
+        }
+    }
+
+    input.onblur = () => {
+        const inputMsg = document.querySelectorAll('.input-msg');
+        if (input.ariaValueMax.length == 0) {
+            inputMsg[i].innerHTML = '<i class="fa-solid fa-circle-xmark"></i>';
+        } else {
+            inputMsg[i].innerHTML = '<i class="fa-solid fa-circle-check" style= "color: #828282"></i>';
+        }
+    }
+}
