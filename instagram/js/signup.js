a7e7ef33806274ed679dbbc989d421ee0af27d6e
= <ITPS@gmail.com>
2022-04-01 12:59:25 +0900

    Html, CSS, JS 수업(수정)

--- instagram/js/signup.js
+++ instagram/js/signup.js
@@ -1,22 +1,23 @@
 const input_datas = document.querySelectorAll('.input-data');
-const password_visible = document.querySelector('.password-visible')
+const password_visible = document.querySelector('.password-visible');
 
 password_visible.onclick = () => {
 
 }
 
+
 for (let i = 0; i < input_datas.length; i++) {
     const label = input_datas[i].querySelector('label');
-    const input = input_datas[i].querySelector('label');
+    const input = input_datas[i].querySelector('input');
 
-    label.onkeydown = () => {
+    input.onkeydown = () => {
         label.style.top = '2px';
         label.style.fontSize = '11px';
         input.style.padding = '16px 0px 2px 8px';
     }
 
     input.onkeyup = () => {
-        if (input.ariaValueMax.length == 0) {
+        if (input.value.length == 0) {
             label.style.top = '9px';
             label.style.fontSize = '13px';
             input.style.padding = '9px 0px 7px 8px';
@@ -25,10 +26,10 @@ for (let i = 0; i < input_datas.length; i++) {
 
     input.onblur = () => {
         const inputMsg = document.querySelectorAll('.input-msg');
-        if (input.ariaValueMax.length == 0) {
-            inputMsg[i].innerHTML = '<i class="fa-solid fa-circle-xmark"></i>';
+        if (input.value.length == 0) {
+            inputMsg[i].innerHTML = `<i class="fa-solid fa-circle-xmark"></i>`;
         } else {
-            inputMsg[i].innerHTML = '<i class="fa-solid fa-circle-check" style= "color: #828282"></i>';
+            inputMsg[i].innerHTML = `<i class="fa-solid fa-circle-check" style="color: #8e8e8e;"></i>`;
         }
     }
 }
